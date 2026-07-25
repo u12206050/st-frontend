89868e9e5075b98ff377a3b91225244b1cd692df
Fredrik Vedvik <fredrik@vedvik.tech>
2026-07-25 14:16:12 +0200

    revert api basepath

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const firebaseConfig = {
   measurementId: "G-YWMEW6TKB8",
 };
 
-const basePath = "https://st-api-44114060540.europe-west4.run.app/";
+const basePath = "https://songtreasures.azurewebsites.net/";
 
 const config = {
   api: {
